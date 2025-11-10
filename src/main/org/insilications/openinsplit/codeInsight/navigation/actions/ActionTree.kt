ee4e1553c341c0da8e024ed410017740ec5c01bd
Francisco Boni <boboniboni@gmail.com>
2025-11-10 09:07:37 -0300

    Small fixes & housekeeping.

--- src/main/org/insilications/openinsplit/codeInsight/navigation/actions/ActionTree.kt
+++ src/main/org/insilications/openinsplit/codeInsight/navigation/actions/ActionTree.kt
@@ -1,5 +1,7 @@
 package org.insilications.openinsplit.codeInsight.navigation.actions
 
+import com.intellij.codeInsight.TargetElementUtil
+import com.intellij.ide.structureView.StructureViewTreeElement
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
@@ -8,10 +10,12 @@ import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import org.insilications.openinsplit.debug
-import org.jetbrains.kotlin.idea.refactoring.project
-
+import org.jetbrains.kotlin.idea.k2.codeinsight.structureView.KotlinFirStructureViewElement
+import org.jetbrains.kotlin.psi.*
+import org.jetbrains.kotlin.psi.psiUtil.containingClassOrObject
 
 class ActionTree : DumbAwareAction() {
     companion object {
@@ -30,7 +34,7 @@ class ActionTree : DumbAwareAction() {
         LOG.debug { "ActionTree - actionPerformed" }
 
         val dataContext: DataContext = e.dataContext
-        val project: Project = dataContext.project
+//        val project: Project = dataContext.project
         val editor: Editor? = CommonDataKeys.EDITOR.getData(dataContext)
         val file: PsiFile? = CommonDataKeys.PSI_FILE.getData(dataContext)
 
@@ -42,7 +46,87 @@ class ActionTree : DumbAwareAction() {
         val offset: Int = editor.caretModel.offset
 
         LOG.debug { "ActionTree - actionPerformed - offset: $offset" }
+
+        val targetElementUtilInstance: TargetElementUtil = TargetElementUtil.getInstance()
+        val targetElementUtilAllAccepted: Int = targetElementUtilInstance.allAccepted
+        // Find the semantic target element at the caret `offset`
+        val targetElement: PsiElement? = targetElementUtilInstance.findTargetElement(editor, targetElementUtilAllAccepted, offset)
+
+        if (targetElement == null) {
+            e.presentation.isEnabled = false
+            LOG.info("actionPerformed - targetElement == null")
+            return
+        }
+
+        LOG.info("actionPerformed - targetElement: ${targetElement.text}")
+        val project: Project = targetElement.project
+
+        val targetElementStructureViewTreeElementList: Collection<StructureViewTreeElement> = file.getStructureViewChildren {
+            KotlinFirStructureViewElement(it, it, isInherited = false)
+        }
+        LOG.debug { "ActionTree - actionPerformed - targetElementStructureViewTreeElementList size: ${targetElementStructureViewTreeElementList.size}" }
+        for (element in targetElementStructureViewTreeElementList) {
+            LOG.info("actionPerformed - targetElementStructureViewTreeElement: ${element.presentation.presentableText}")
+        }
+//        val targetElementStructureViewTreeElementList: Collection<StructureViewTreeElement> = targetElement.getStructureViewChildren { decl ->
+//            KotlinStructureViewElement(decl, isInherited = false)
+//        }
+//        LOG.debug { "ActionTree - actionPerformed - targetElementStructureViewTreeElementList size: ${targetElementStructureViewTreeElementList.size}" }
+//        for (element in targetElementStructureViewTreeElementList) {
+//            LOG.info("actionPerformed - targetElementStructureViewTreeElement: ${element.presentation.presentableText}")
+//        }
+    }
+}
+
+fun isStructureViewNode(element: PsiElement?): Boolean =
+    element is KtDeclaration &&
+            element !is KtPropertyAccessor &&
+            element !is KtFunctionLiteral &&
+            !(element is KtParameter && !element.hasValOrVar()) &&
+            !((element is KtProperty || element is KtFunction) && !element.topLevelDeclaration && element.containingClassOrObject !is KtNamedDeclaration)
+
+private val KtDeclaration.topLevelDeclaration: Boolean
+    get() = parent is PsiFile || parent is KtBlockExpression && parent.parent is KtScript
+
+fun PsiElement.getStructureViewChildren(factory: (KtDeclaration) -> StructureViewTreeElement): Collection<StructureViewTreeElement> {
+    val children = when (val element = this) {
+        is KtCommonFile -> {
+            val declarations = element.declarations
+            if (element.isScript()) {
+                (declarations.singleOrNull() as? KtScript) ?: element
+            } else {
+                element
+            }.declarations
+        }
+
+        is KtClass -> element.getStructureDeclarations()
+        is KtClassOrObject -> element.declarations
+        is KtFunction, is KtClassInitializer, is KtProperty -> element.collectLocalDeclarations()
+        else -> emptyList()
     }
 
-
+    return children.map { factory(it) }
 }
+
+private fun PsiElement.collectLocalDeclarations(): List<KtDeclaration> {
+    val result = mutableListOf<KtDeclaration>()
+
+    acceptChildren(object : KtTreeVisitorVoid() {
+        override fun visitClassOrObject(classOrObject: KtClassOrObject) {
+            result.add(classOrObject)
+        }
+
+        override fun visitNamedFunction(function: KtNamedFunction) {
+            result.add(function)
+        }
+    })
+
+    return result
+}
+
+fun KtClassOrObject.getStructureDeclarations(): List<KtDeclaration> =
+    buildList {
+        primaryConstructor?.let { add(it) }
+        primaryConstructorParameters.filterTo(this) { it.hasValOrVar() }
+        addAll(declarations)
+    }
